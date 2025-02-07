bb401b13c3b4475202f19f3a25551355ee61f21e
Nadia Lutz <na.lutz@outlook.com>
2025-02-07 13:18:34 +0100

    think its done

--- destination.js
+++ destination.js
@@ -1,24 +1,82 @@
+
+
 let search = window.location.search;
 let params = new URLSearchParams(search);
-let id = params.get('id');
-
+let id = params.get("id");
+console.log(id);
+ 
 fetch(`/data/${id}.json`)
-.then(response => response.json())
-.then(data => {
+    .then(response => response.json())
+    .then(data => {
+ 
+        let detailSection = document.querySelector(".destination");
+ 
+            let div = document.createElement("div");
+            div.classList.add("destination__div");
+            div.innerHTML = `
+                <img src="img/${data.image}">
+               
+                <div>
+                    <h1>${data.destination}</h1>
+                    <h2>${data.title}</h2>
+                    <h3>${data.subtitle}</h3>
+                    <p>${data.text}</p>
+                    <h3 class="faci__h3">${"Faciliteter"}</h3>
+                <ul>
+                    <li>${data.facilities[0]}</li>
+                    <li>${data.facilities[1]}</li>
+                    <li>${data.facilities[2]}</li>
+                    <li>${data.facilities[3]}</li>
+                </ul>
+                </div>
+            `
+            ;
+        detailSection.append(div)
+        console.log(detailSection);
+        
+    })
 
-    let body = document.body;
+
+
+// let search = window.location.search;
+// let params = new URLSearchParams(search);
+// let id = params.get('id');
+
+// fetch(`/data/${id}.json`)
+// .then(response => response.json())
+// .then(data => {
+
+
+//     let body = document.body;
+//     body.classList.add("destination")
     
-    let title = document.createElement("h1");
-    title.textContent = data.title;
-    body.appendChild(title);
+//     let img = document.createElement("img");
+//     img.src = `img/${data.image}`;
+//     body.appendChild(img);
+
+//     let container = document.createElement('div');
+//     container.classList.add("div");
+//     body.appendChild(container);
+
+//     let title = document.createElement("h1");
+//     title.textContent = data.title;
+//     container.appendChild(title);
     
-    let img = document.createElement("img");
-    img.src = `img/${data.image}`;
-    img.alt = data.title;
-    body.appendChild(img);
-    
-    let description = document.createElement("p");
-    description.textContent = data.text;
-    body.appendChild(description);
-    
-});
+//     let description = document.createElement("p");
+//     description.textContent = data.text;
+//     container.append(description);
+// });
+
+// let container = document.createElement("div");
+// container.classList.add("destination");
+
+// container.innerHTML = `
+//    ${data.destinations.facilities.map(facilities => `
+//        <li>
+           
+//        </li>`).join("")}
+// `;
+
+// sectionElm.appendChild(listElm);
+
+
--- script.js
+++ script.js
@@ -1,20 +1,33 @@
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     fetch(`/data/destinations.json`)
     .then(response => response.json())
     .then(function(data) {
+        
         let sectionElm = document.querySelector("section");
+        
+        let sectionHeadline = document.createElement("h1");
+        sectionHeadline.textContent = "Apartments for Rent";
+        sectionElm.appendChild(sectionHeadline);
+
+
         let listElm = document.createElement("ul");
-        listElm.classList.add("details");
+        listElm.classList.add("ul");
 
         listElm.innerHTML = `
            ${data.destinations.map(destinations => `
                <li>
-                   <a href="destination.html?id=${destinations.id}">${destinations.destination}</a>
                    <img src="img/${destinations.image}" alt="${destinations.title}"> 
-                   <button class="more-button" onclick="location.href='destination.html?id=${destinations.id}'">More</button>
+                   <div class="button__container">
+                       <img src="img/heart.png" class="heart-icon" alt="Heart Icon">
+                       <button class="more-button" onclick="location.href='destination.html?id=${destinations.id}'">MORE</button>
+                   </div>
                </li>`).join("")}
         `;
         
-        sectionElm.appendChild(listElm);
+        sectionElm.append(listElm);
     });
+
 });
